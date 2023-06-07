95dd306ae1c0b337905c22048799a28d21253e74
João Victor <freitasjoaovictor49@gmail.com>
2023-06-07 13:29:04 -0300

    feat: adding pause menu and timer component

---
+++ lib/screens/game/components/timer.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class Countdown extends StatefulWidget {
+  final String durationString;
+
+  const Countdown({
+    required Key key,
+    required this.durationString
+  }) : super(key: key);
+
+  @override
+  CountdownState createState() => CountdownState();
+}
+
+class CountdownState extends State<Countdown> {
+  late Duration duration;
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    duration = parseDurationString(widget.durationString);
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() {
+        if (duration.inSeconds > 0) {
+          duration = duration - const Duration(seconds: 1);
+        } else {
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  void pauseTimer() {
+    timer.cancel();
+  }
+
+  void resumeTimer() {
+    startTimer();
+  }
+
+  Duration parseDurationString(String durationString) {
+    final parts = durationString.split(':');
+    final hours = int.parse(parts[0]);
+    final minutes = int.parse(parts[1]);
+    final seconds = double.parse(parts[2]).toInt();
+    return Duration(hours: hours, minutes: minutes, seconds: seconds);
+  }
+
+  String formatDuration(Duration duration) {
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes.remainder(60);
+    final seconds = duration.inSeconds.remainder(60);
+    return '$hours:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            formatDuration(duration),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24
+            ),
+          )
+        ]
+      )
+    );
+  }
+}
--- lib/screens/game/game.dart
+++ lib/screens/game/game.dart
@@ -4,6 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:futebol_app/hive/games.dart';
 import 'package:hive/hive.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+import 'components/timer.dart';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -14,6 +17,7 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  final GlobalKey<CountdownState> _timerKey = GlobalKey<CountdownState>();
   Map<String, dynamic>? args;
   late Box gamesBox;
   
@@ -30,6 +34,16 @@ class _GameState extends State<Game> {
     setState(() {
       paused = true;
     });
+    _timerKey.currentState?.pauseTimer();
+    showMenu();
+  }
+
+  void resume() {
+    Navigator.pop(context);
+    setState(() {
+      paused = false;
+    });
+    _timerKey.currentState?.resumeTimer();
   }
 
   void getInfos() {
@@ -48,6 +62,101 @@ class _GameState extends State<Game> {
       // ignore: avoid_print
       print('[ ERROR: Missing game id on game start ]');
     }
+  }
+
+  void showMenu() {
+    showMaterialModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isDismissible: false,
+      enableDrag: false,
+
+      builder: (context) => Center(
+        child: Container(
+          width: ScreenUtil().screenWidth,
+          height: ScreenUtil().screenHeight,
+          color: Colors.transparent,
+          child: Stack(
+            children: [
+              Positioned(
+                top: ScreenUtil().screenHeight * .25/2,
+                left: ScreenUtil().screenWidth * .25,
+                child: Container(
+                  width: ScreenUtil().screenWidth * .5,
+                  height: ScreenUtil().screenHeight * .75,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: resume,
+                            child: const SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child: FaIcon(
+                                  FontAwesomeIcons.xmark,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 50),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 80),
+                        child: ElevatedButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            '+1 min acréscimo',
+                            style: TextStyle(
+                              fontSize: 18
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 80,
+                          vertical: 20
+                        ),
+                        child: OutlinedButton(
+                          onPressed: () {
+                          },
+                          style: ButtonStyle(
+                            side: MaterialStateProperty.all<BorderSide>(
+                              const BorderSide(
+                                width: 2.0,
+                                color: Colors.red
+                              )
+                            ),
+                          ),
+                          child: const Text(
+                            'Encerrar Partida',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.red
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -158,13 +267,10 @@ class _GameState extends State<Game> {
                       bottomRight: Radius.circular(35),
                     )
                   ),
-                  child: const Center(
-                    child: Text(
-                      '00:35',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w800
-                      ),
+                  child: Center(
+                    child: Countdown(
+                      key: _timerKey,
+                      durationString: duration,
                     ),
                   ),
                 )
